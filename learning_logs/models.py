1d4436fdea5610b0e63cdb55ebbd2d112115d74a
MVNDAY <83238300+MVNDAY@users.noreply.github.com>
2021-04-26 20:46:26 +0300

    First Commit

---
+++ learning_logs/models.py
@@ -0,0 +1,23 @@
+from django.db import models 
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Topic(models.Model):
+	text = models.CharField(max_length = 200)
+	date_added = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User,on_delete=models.CASCADE)
+	def __str__(self):
+		return self.text
+
+class Entry(models.Model):
+	"""user's info about the theme"""
+	topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entires'
+
+	def __str__(self):
+		"""returns str view of the model"""
+		return self.text[:50] + "..."
